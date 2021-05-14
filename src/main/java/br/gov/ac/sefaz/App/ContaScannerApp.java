eeba7ced3f89a278739fc6337e616a34b7d45400
Roberto-Moraes <endriosrobert@gmail.com>
2021-05-14 16:03:24 -0500

    Foiiiiii

--- src/main/java/br/gov/ac/sefaz/App/ContaScannerApp.java
+++ src/main/java/br/gov/ac/sefaz/App/ContaScannerApp.java
@@ -9,13 +9,13 @@ import br.gov.ac.sefaz.conta.ContaSalario;
 
 public class ContaScannerApp {
 	public static void main(String[] args) {
-		PessoaFisca pessoaF = new  PessoaFisca();
+		PessoaFisca pessoaF = new PessoaFisca();
 		PessoaJuridica pessoaJ = new PessoaJuridica();
-		ContaCorrente corrente =new  ContaCorrente();
+		ContaCorrente corrente = new ContaCorrente();
 		ContaSalario salario = new ContaSalario();
-		
+
 		Scanner entrada = new Scanner(System.in);
-		
+
 		System.out.println("***** Banco Mentorama ** R$ **************");
 		System.out.println("Menu de Opção para você Cliente amigo !");
 		System.out.println(" 1 - Criar Conta ");
@@ -26,29 +26,74 @@ public class ContaScannerApp {
 		System.out.println(" 6 - Mostrar montante disponivel nas contas");
 		System.out.println(" 7 - sair");
 		System.out.println(" Escolha a opção desejada :");
-		
+
 		System.out.println("Digita sua Opção : ");
 		int opcao = entrada.nextInt();
-		
+
 		switch (opcao) {
 		case 1:
+			Scanner tconta = new Scanner(System.in);
+			System.out.println("Tipo de Conta");
+			int val = tconta.nextInt();
+			System.out.println();
+			System.out.println(val);
+			break;
+		case 2:
+			Scanner valor = new Scanner(System.in);
+			ContaSalario salario1 = new ContaSalario();
+			pessoaF.setNome("Roberto Moraes");
+			pessoaF.setIdade("30");
+			pessoaF.setRg("12134567-8");
+			pessoaF.setCpf("000.000.000-00");
+			salario1.setAg("1234-5");
+			salario1.setConta("0001245-7");
+			salario1.setSaldo(258.45);
+			
+			System.out.println("Sacar :");
+			double valo = valor.nextDouble();
+			salario1.sacarSalario(salario.getSaldo(), valo);
+			System.out.println(salario1.getSaldo());
+
+			valor.close();
+			break;
+		case 3:
+			Scanner valor1 = new Scanner(System.in);
+			ContaSalario salario2 = new ContaSalario();
+			pessoaF.setNome("Roberto Moraes");
+			pessoaF.setIdade("30");
+			pessoaF.setRg("12134567-8");
+			pessoaF.setCpf("000.000.000-00");
+			salario2.setAg("1234-5");
+			salario2.setConta("0001245-7");
+			salario2.setSaldo(258.45);
+			
+			System.out.println("Depositar :");
+			double valo2 = valor1.nextDouble();
+			salario2.depositarSalario(salario.getSaldo(), valo2);
+			System.out.println("Parabéns pelo seu Depostio !:R$"salario2.getSaldo());
+
+			valor1.close();
+			break;
+		case 4:
 			System.out.println("Conta Pessoa Fisica");
-				System.out.println("Digite o nome ?");
-				System.out.println("Idade ?");
-				System.out.println("Rg ?");
+			System.out.println("Digite o nome ?");
+			System.out.println("Idade ?");
+			System.out.println("Rg ?");
 			break;
 
-		case 2:
-			ContaSalario salario1 = new ContaSalario();
-			Scanner valor = new Scanner(System.in);
-			double val = valor.nextDouble();
-			System.out.println("Sacar :");
-			salario1.sacarSalario(salario.getSaldo(), val);
+		case 5:
+
 			break;
+
+		case 6:
+
+			break;
+
 		default:
+			System.out.println("Sair do programa");
 			break;
 		}
-		
+
 		entrada.close();
 	}
 }
